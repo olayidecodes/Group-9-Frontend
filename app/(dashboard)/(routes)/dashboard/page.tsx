6a8a2787bd444eb0688139add35a476206226a46
VICTORIA MATTHEWS <olayidecodes@gmail.com>
2023-08-28 13:54:19 +0100

    added routes

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <p>Dashboard Page (Protected)</p>
+    </div>
+  )
+}
+
+export default DashboardPage
